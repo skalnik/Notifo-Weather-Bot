e6234d1722b5cc53b40151c12a3b4ff41c0b6f7d
Mike Skalnik <mike.skalnik@gmail.com>
2010-09-30 16:00:37 -0400

    Responds to webhook

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'notifo'
+require 'yaml'
+
+configure do
+  config = YAML::load_file('config.yml')
+  @notifo = Notifo.new(config[username], config[password]);
+end
 
 get '/' do
-  "Hello, World!"
+  "This is a notifo bot that reports the weather. Simply message your zip code to 'weather'!"
+end
+
+post '/fetch' do
+  if notifo.verify_webhook_signature(params)
+    notifo.post(params[:notifo_from_username], 'Hello, World!')
+  end
 end
